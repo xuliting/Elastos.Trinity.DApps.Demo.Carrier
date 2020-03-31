c5a08829a12298762f718e42c7b45c46a3f45acd
Benjamin Piette <benjamin.piette@elastos-fusion.org>
2020-03-31 13:56:07 +0800

    Added news parameters to publish script

--- store/publish.sh
+++ store/publish.sh
@@ -1,1 +1,1 @@
-trinity-cli publish --did did:elastos:idE7pyBGGXtJR8QJLYHnaA9fjKmuzoYpfU#primary --didstore ../DAppSignatures/org.elastos.trinity.dapp.carrierdemo/appdid/
+trinity-cli publish --news $1 --did did:elastos:idE7pyBGGXtJR8QJLYHnaA9fjKmuzoYpfU#primary --didstore ../DAppSignatures/org.elastos.trinity.dapp.carrierdemo/appdid/
